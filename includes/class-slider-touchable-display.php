a976ea86e4374509edee86996125c0dc75e2a151
LeoCR <leonardoas2007@gmail.com>
2022-12-12 21:56:13 -0600

    fix: Defining Styles

--- includes/class-slider-touchable-display.php
+++ includes/class-slider-touchable-display.php
@@ -4,6 +4,7 @@ class SliderTouchable_Display {
 		add_filter( 'the_content', array( $this, 'display_notice' ) );
 		add_shortcode( 'slider_touchable', array( $this, 'slider_touchable_func') );
 		add_action('wp_footer', array( $this, 'footer_scripts'));
+		add_action('wp_head',array($this,'header_scripts'));
 	}
 	public function display_notice( $content ) {
 		$notice = get_post_meta( get_the_ID(), 'slider_touchable', true );
@@ -14,6 +15,13 @@ class SliderTouchable_Display {
 			$content = $notice_html . $content;
 		}
 		return $content;
+	}
+	public function header_scripts(){
+		?>
+		<style type="text/css" media="all" id='swiper-slider">
+			<?php echo wp_remote_fopen(plugin_dir_url( __FILE__ ). '../assets/css/dev/swiper.css');?>
+		</style>
+		<?php
 	}
 	public function footer_scripts()
 {
@@ -52,15 +60,24 @@ class SliderTouchable_Display {
 			if(get_post_meta($image->ID,"slider_touchable_object_settings",true)!==''){
 				$sliderTouchableJsonSettings=json_decode( get_post_meta($image->ID,"slider_touchable_object_settings",true));
 			}
-		} 
-		$sliderTouchableJS.='<script data-defer="defer" id="swiper" src="'.get_stylesheet_directory_uri() . '/assets/js/vendors/swiper.min.js"></script>';
+		}
+		$sliderTouchableJS.='<script id="swiper-lib-js" data-defer="defer" src="'.plugin_dir_url( __FILE__ ). '../assets/js/swiper.min.js'.'"></script>';
 		$sliderTouchableJS.='<script data-defer="defer" id="swiper-slider">';
 		$sliderTouchableJS.='const jsonSettings='.get_post_meta($image->ID,"slider_touchable_object_settings",true).';';
+		 
+		$sliderTouchableJsonSettings=json_decode(get_post_meta($image->ID,"slider_touchable_object_settings",true));
 		$sliderTouchableJS.='let hasAutoPlay=jsonSettings.settings.hasAutoplay;';
 		$sliderTouchableJS.='if (typeof Swiper !== undefined) {';
 		$sliderTouchableJS.='if (hasAutoPlay) {}';
 		$sliderTouchableJS.='var swiperSlider = new Swiper("#slider-touchable-'.$sqlSliderTouchable[0]->ID.'", {';
 		$sliderTouchableJS.='loop: true,effect: "cube",grabCursor: true,';
+		if($sliderTouchableJsonSettings->settings->hasAutoplay){
+			$sliderTouchableJS.='autoplay: {
+				delay: '.$sliderTouchableJsonSettings->settings->autoplay->duration.',
+				disableOnInteraction: false,
+			  },';
+		}
+		
 		$sliderTouchableJS.='cubeEffect: {';
 		$sliderTouchableJS.='shadow: true,slideShadows: true,shadowOffset: 20,';
 		$sliderTouchableJS.='shadowScale: 0.94,},';
@@ -120,10 +137,80 @@ class SliderTouchable_Display {
 						}
 						$sliderTouchable .="
 				</div>
-				<div class='swiper-button-next'></div>
-				<div class='swiper-button-prev'></div>
-			</div>
-			";
+				<div class='swiper-button-prev btn-prev'>
+                    <svg style='fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:square;stroke-miterlimit:1.5;max-width:70px' width='100%' height='100%' viewBox='0 0 272 276' version='1.1' xmlns='http://www.w3.org/2000/svg'>
+                        <g transform='matrix(1,0,0,1,-13.8912,-10.9071)'>
+                            <g transform='matrix(4.1707,0,0,4.1707,-5072.11,-1174.15)'>
+                                <g class='gray_elipse' transform='matrix(0.397005,0,0,0.376374,1251.86,-298.781)'>
+                                    <ellipse cx='0.259' cy='1638.91' rx='81.401' ry='84.792' style='fill:url(#bg_btn_linear_gray);stroke:rgb(108,108,108);stroke-width:0.53px'></ellipse>
+                                </g>
+                            </g>
+                            <g transform='matrix(4.1707,0,0,4.1707,-5072.11,-1174.15)'>
+                                <g class='bg_normal' transform='matrix(0.38389,0,0,0.367676,1252.03,-286.839)'>
+                                    <ellipse cx='0.259' cy='1638.91' rx='81.401' ry='84.792' style='fill:#ffffff;stroke:#ffffff;stroke-width:1.28px'></ellipse>
+                                </g>
+                            </g>
+                            <g transform='matrix(4.1707,0,0,4.1707,-5072.11,-1174.15)'>
+                                <g transform='matrix(0.63116,1.52847e-16,-1.52847e-16,0.61693,1214.93,-682.097)'>
+                                    <path d='M73.637,1649.99L26.725,1618.63L78.779,1584.29L61.028,1617.77L73.637,1649.99Z' style='fill:rgb(75,75,75);stroke:rgb(76,76,76);stroke-width:0.13px;stroke-linejoin:bevel'></path>
+                                </g>
+                            </g>
+                            <g transform='matrix(4.1707,0,0,4.1707,-5072.11,-1174.15)'>
+                                <g transform='matrix(0.63116,1.52847e-16,-1.52847e-16,0.61693,1215.32,-685.153)'>
+                                    <path d='M73.29,1655.09L28.139,1623.03L79.931,1587.67L59.202,1622.46L73.29,1655.09Z' style='stroke:black;stroke-width:0.13px;stroke-linejoin:bevel'></path>
+                                </g>
+                            </g>
+                        </g>
+                        <defs>
+                            <linearGradient id='bg_btn_linear_gray' x1='0' y1='0' x2='1' y2='0' gradientUnits='userSpaceOnUse' gradientTransform='matrix(162.802,0,0,169.583,-81.1418,1638.91)'>
+                                <stop offset='0' style='stop-color:black;stop-opacity:1'></stop>
+                                <stop offset='1' style='stop-color:rgb(160,160,160);stop-opacity:1'></stop>
+                            </linearGradient>
+                            <radialGradient id='bg_btn_radial_orange' cx='0' cy='0' r='1' gradientUnits='userSpaceOnUse' gradientTransform='matrix(75.3738,0,0,78.6976,5.53429,1631.77)'>
+                                <stop offset='0' style='stop-color:rgb(255,141,53);stop-opacity:1'></stop>
+                                <stop offset='1' style='stop-color:rgb(255,161,88);stop-opacity:1'></stop>
+                            </radialGradient>
+                        </defs>
+                    </svg>
+                </div>
+                <div class='swiper-button-next btn-next'>
+                    <svg width='100%' height='100%' viewBox='0 0 272 276' version='1.1' xmlns='http://www.w3.org/2000/svg' style='fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:square;stroke-miterlimit:1.5;max-width:70px'>
+                        <g transform='matrix(1,0,0,1,-13.8912,-10.9071)'>
+                            <g transform='matrix(4.1707,0,0,4.1707,-5072.11,-1174.15)'>
+                                <g class='gray_elipse' transform='matrix(0.397005,0,0,0.376374,1251.86,-298.781)'>
+                                    <ellipse cx='0.259' cy='1638.91' rx='81.401' ry='84.792' style='fill:url(#bg_btn_linear_gray);stroke:rgb(108,108,108);stroke-width:0.53px'></ellipse>
+                                </g>
+                            </g>
+                            <g transform='matrix(4.1707,0,0,4.1707,-5072.11,-1174.15)'>
+                                <g class='bg_normal' transform='matrix(0.38389,0,0,0.367676,1252.03,-286.839)'>
+                                    <ellipse cx='0.259' cy='1638.91' rx='81.401' ry='84.792' style='fill:#ffffff;stroke:#ffffff;stroke-width:1.28px'> 
+                                    </ellipse>
+                                </g>
+                            </g>
+                            <g transform='matrix(4.1707,0,0,4.1707,-5072.11,-1174.15)'>
+                                <g id='gray_arrow' transform='matrix(-0.63116,-2.30142e-16,2.28399e-16,-0.61693,1287.43,1312.35)'>
+                                    <path d='M73.637,1649.99L26.725,1618.63L78.779,1584.29L61.028,1617.77L73.637,1649.99Z' style='fill:rgb(75,75,75);stroke:rgb(76,76,76);stroke-width:0.13px;stroke-linejoin:bevel'></path>
+                                </g>
+                            </g>
+                            <g transform='matrix(4.1707,0,0,4.1707,-5072.11,-1174.15)'>
+                                <g id='black_arrow' transform='matrix(-0.63116,-2.30142e-16,2.28399e-16,-0.61693,1287.04,1315.4)'>
+                                    <path d='M73.29,1655.09L28.139,1623.03L79.931,1587.67L59.202,1622.46L73.29,1655.09Z' style='stroke:black;stroke-width:0.13px;stroke-linejoin:bevel'></path>
+                                </g>
+                            </g>
+                        </g>
+                        <defs>
+                            <linearGradient id='bg_btn_linear_gray' x1='0' y1='0' x2='1' y2='0' gradientUnits='userSpaceOnUse' gradientTransform='matrix(162.802,0,0,169.583,-81.1418,1638.91)'>
+                                <stop offset='0' style='stop-color:black;stop-opacity:1'></stop>
+                                <stop offset='1' style='stop-color:rgb(160,160,160);stop-opacity:1'></stop>
+                            </linearGradient>
+                            <radialGradient id='bg_btn_radial_orange' cx='0' cy='0' r='1' gradientUnits='userSpaceOnUse' gradientTransform='matrix(75.3738,0,0,78.6976,5.53429,1631.77)'>
+                                <stop offset='0' style='stop-color:rgb(255,141,53);stop-opacity:1'></stop>
+                                <stop offset='1' style='stop-color:rgb(255,161,88);stop-opacity:1'></stop>
+                            </radialGradient>
+                        </defs>
+                    </svg>
+                </div>
+			</div>";
 			$sliderTouchable.=$sliderTouchableJS;
 			$sliderTouchableCSS.="</style>";
 			$sliderTouchable .=$sliderTouchableCSS;
